fa58f89ab117192231c8c7b04d6285834c05dc1a
Kelly Thompson <ksthompson48@gmail.com>
2020-02-19 15:36:09 -0600

    add buttons for previously searched cities

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let prevCities = [];
+
+function renderPrevCities() {
+    $("#prev-cities").empty();
+    for (let i=0; i<prevCities.length; i++) {
+        let cityRow = $("<div>");
+        cityRow.attr("class", "row");
+        let cityCol = $("<div>");
+        cityCol.attr("class", "col-sm-12");
+        let button = $("<button>");
+        button.attr("class", "prev-city");
+        button.text(prevCities[i]);
+
+        $("#prev-cities").append(cityRow);
+        cityRow.append(cityCol);
+        cityCol.append(button);
+    }
+};
+
 $('#search').on('click', function() {
     let location = $('#location').val();
     let APIKey = "b289af46fb389f130dbb6ff895414999";
@@ -15,8 +34,11 @@ $('#search').on('click', function() {
         $('#current-temp').text("Current Temp: "+ response.main.temp);
         $('#current-humid').text("Current Humidity: " + response.main.humidity + "%");
         $('#current-wind').text("Current Wind Speed: " + response.wind.speed);
+        let city = response.name;
+        prevCities.push(city);
+        renderPrevCities();
     });
-    
+
     $.ajax({
         url: forecastURL,
         method: "GET"
